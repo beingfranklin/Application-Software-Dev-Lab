af59d7c1324ebbc51adde2437d60432cb1008bb5
Franklin Antony <franklinanton.007@gmail.com>
2017-10-11 16:26:15 +0530

    DAY 8 PL/SQL

--- PL_SQL_Func3.sql
+++ PL_SQL_Func3.sql
@@ -1,6 +1,14 @@
-CREATE OR REPLACE PROCEDURE incComm
-IS 
-BEGIN 
- UPDATE EMPLOYEE
- SET COMMISSION = COMMISSION + 0.05*COMMISSION ;
-END;
+DECLARE
+	s varchar2(20);
+	s_read varchar2(20);
+
+	PROCEDURE increment(s IN varchar2)
+	IS
+		BEGIN
+			update EMPLOYEE set COMMISSION=1.05*COMMISSION where EMPNAME=s;
+		END;
+	BEGIN
+ 		s_read:='&Name';
+		increment(s_read);
+	END;
+    
